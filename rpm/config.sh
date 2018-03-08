e4aa6f4ff4e1c377511d703e199f315599573a21
Michal Suchanek <msuchanek@suse.de>
2018-03-08 11:09:44 +0100

    config.sh: build against SLE-15 in OBS.

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,10 +3,7 @@ SRCVERSION=4.12
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:Factory
-OBS_PROJECT_ARM=openSUSE:Factory:ARM
-OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
-OBS_PROJECT_S390=openSUSE:Factory:zSystems
+OBS_PROJECT=SUSE:SLE-15:GA
 IBS_PROJECT=SUSE:SLE-15:GA
 # Bugzilla info
 BUGZILLA_SERVER="apibugzilla.suse.com"
