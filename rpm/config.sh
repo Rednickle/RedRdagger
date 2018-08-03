2813d45bcb18b1d3819198e3c04fe60bfdbf6642
Takashi Iwai <tiwai@suse.de>
2018-08-03 09:15:41 +0200

    rpm/config.sh: Use SUSE:SLE-15:Update project instead of GA

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,8 +3,8 @@ SRCVERSION=4.12
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=SUSE:SLE-15:GA
-IBS_PROJECT=SUSE:SLE-15:GA
+OBS_PROJECT=SUSE:SLE-15:Update
+IBS_PROJECT=SUSE:SLE-15:Update
 # Bugzilla info
 BUGZILLA_SERVER="apibugzilla.suse.com"
 BUGZILLA_PRODUCT="SUSE Linux Enterprise Server 15"
