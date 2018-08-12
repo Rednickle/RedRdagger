c157db3e19796c379df7676aeeb6abbd09a4f702
Olaf Hering <ohering@suse.de>
2018-08-12 21:11:33 +0200

    rpm/config.sh: set VARIANT=-azure, for kernel-source

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,7 +1,7 @@
 # The version of the main tarball to use
 SRCVERSION=4.12
-# variant of the kernel-source package, either empty or "-rt"
-VARIANT=
+# variant of the kernel-source package, either empty or "-rt" or "-azure"
+VARIANT=-azure
 # buildservice projects to build the kernel against
 OBS_PROJECT=SUSE:SLE-12-SP4:GA
 IBS_PROJECT=SUSE:SLE-12-SP4:GA
