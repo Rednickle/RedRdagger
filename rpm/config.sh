7bc9e50eeaf90600b3cd2de16d1a67c6f546e4de
Petr Tesarik <ptesarik@suse.cz>
2018-12-07 18:06:21 +0100

    - README.BRANCH: Change product to openSUSE 15.1 - rpm/config.sh: Build in openSUSE:Leap:15.1

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,8 +3,7 @@ SRCVERSION=4.12
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=SUSE:SLE-15:Update
-IBS_PROJECT=SUSE:SLE-15:Update
+OBS_PROJECT=openSUSE:Leap:15.1
 # Bugzilla info
 BUGZILLA_SERVER="apibugzilla.suse.com"
 BUGZILLA_PRODUCT="SUSE Linux Enterprise Server 15"
